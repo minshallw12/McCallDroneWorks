65c1a23e8df7eb5fdddeda5471281d636d3a2d1b
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-05-22 11:20:48 -0600

    started about me page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,31 @@
+import Headshot from '../images/headshot.jpg';
+
 export default function About() {
     return (
-        <div className="About">
-            <h1>About me</h1>
+        <div className="About" style={{display: 'flex'}}>
+
+            <div className="text_container" style={{border: '2px dotted black'}}>
+                <h3>Will Minshall -- UAV Pilot</h3>
+                <p>
+                    Will is a licensed drone pilot and has 
+                    flown several of the most common types 
+                    of consumer-commercial drones including 
+                    the DJI Phantom 4 Pro, the DJI Mavic 
+                    Air, and the DJI Mavic 2 Pro.
+                </p>
+                <p>
+                    Will has a license in real estate and has 
+                    helped advertise 100+ properties using his 
+                    drones. He loves exploring nature in his 
+                    hometown of McCall, fishing in Payette Lake, 
+                    and spending time with his family.
+                </p>
+            </div>
+
+            <div className="img_container">
+                <img src={Headshot} alt="" style={{maxHeight:'500px', maxWidth:'500px'}}/>
+            </div>
+
         </div>
     )
 }
