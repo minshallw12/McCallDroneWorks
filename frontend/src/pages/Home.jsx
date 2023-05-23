9f126faf490aef0a30021c988cd50514518c57e4
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-05-22 18:57:22 -0600

    Framed homepage.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,39 @@
 export default function Home() {
     return (
         <div className="Home">
-            <h1>Home page</h1>
+            <div className="center">
+                <img src="" alt="" style={{width: '900px', height:'650px'}}/>
+            </div>
+            <div style={{display:'flex', margin: '50px'}}>
+                <div>
+                    <h2>Your eyes in the sky.</h2>
+                    <p>
+                        We are your aerial photography specialists. 
+                        Our fleet of UAV's are commercial grade 
+                        and state of the art. Our pilots are licensed 
+                        with the FAA and have completed stringent 
+                        education and exam requirements. We are 
+                        licensed and insured!
+                    </p>
+                    <span>Our services include:</span>
+                    <ul>
+                        <li>Real Estate advertising photos</li>
+                        <li>Real Estate construction photos</li>
+                        <li>Tower inspections</li>
+                        <li>Industrial inspection</li>
+                        <li>Agriculture/ Land mapping</li>
+                        <li>Commercial & Film *COMING SOON*</li>
+                    </ul>
+                </div>
+                <div>
+                    <img src="" alt="" style={{width: '500px', height:'400px'}}/>
+                </div>
+            </div>
+            <div style={{display: 'flex', justifyContent:'center'}}>
+                <img src="" alt="" style={{width: '450px', height:'500px', padding:'17px'}}/>
+                <img src="" alt="" style={{width: '450px', height:'500px', padding:'17px'}}/>
+            </div>
+            
         </div>
     )
 }
