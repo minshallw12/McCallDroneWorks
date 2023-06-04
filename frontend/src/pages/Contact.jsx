2ef19ea268159a4a80749411ebdd90889ceff978
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-06-03 21:12:38 -0600

    Contact page complete.

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
 import ContactForm from '../components/ContactForm';
+import drone from '../images/EastJugRedline.jpeg'
 
 export default function Contact() {
 
     return (
         <div className="Contact">
-            <div style={{display: 'flex', justifyContent:'center', alignItems:'center', height: '550px', border: '2px dotted black'}}>
-                <h1>Contact Us</h1>
+
+            <div className='center'>
+                <img src={drone} style={{display: 'flex', justifyContent:'center', alignItems:'center', height: '550px'}}/>
             </div>
+            <div className='center' style={{margin: '20px'}}>
+                <h1>Contact Us</h1>
+            </div>  
+
             <div style={{display: 'flex', justifyContent:'center', margin: '20px'}}>
                 <div style={{ display: 'flex', flexDirection:'column', width: '700px', padding: '20px'}}>
                     <div style={{padding: '10px'}}>
@@ -16,7 +22,8 @@ export default function Contact() {
                     <div style={{padding: '10px', paddingTop:'30px'}}>
                         <p>
                             We are friendly and 
-                            service-oriented. Please 
+                            service-oriented. Place an order here,
+                            or
                             leave us a message and 
                             one of our representatives 
                             will get in contact with 
